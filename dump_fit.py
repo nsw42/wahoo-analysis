fe6d586fdb6cab510564e3aa329b82c377a2785d
Neil Walker <neil@wynded.co.uk>
2020-12-05 17:00:10 +0000

    Make it selectable whether to use absolute or relative times

--- dump_fit.py
+++ dump_fit.py
@@ -6,7 +6,10 @@ from analyse import read_input_file
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--offset', action='store_true', dest='show_time_offset',
+                        help="Show times as offset from start")
     parser.add_argument('input', help='Input .fit file')
+    parser.set_defaults(show_time_offset=False)
     args = parser.parse_args()
     return args
 
@@ -15,8 +18,11 @@ def main():
     args = parse_args()
     rows = read_input_file(args.input)
     for row in rows:
-        time_offset = row['timestamp'][0] - rows[0]['timestamp'][0]
-        print('%s  %5u' % (time_offset, row['power'][0]))
+        if args.show_time_offset:
+            timeval = row['timestamp'][0] - rows[0]['timestamp'][0]
+        else:
+            timeval = row['timestamp'][0]
+        print('%s  %5u' % (timeval, row['power'][0]))
 
 if __name__ == '__main__':
     main()
